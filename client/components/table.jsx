b036dc72f022ffc61d49ec35f8a05fbbee38073c
yesseniarod <yessi.rod30@gmail.com>
2021-04-10 00:51:31 +0000

    create a modal that opens when button is clicked. #3

--- client/components/table.jsx
+++ client/components/table.jsx
@@ -18,39 +18,87 @@ class SummaryTable extends React.Component {
 
   render() {
     const calories = calorieCalculator(this.props.gender, this.props.age, this.props.height, this.props.goalWeight, this.props.activityLevel);
-    return (
-      <>
-      <h2 className="table-title">Today</h2>
-        <table>
-          <thead>
-            <tr>
-              <th colSpan="3">{calories} calories remaining</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="border-right">
-                0
+    if (this.state.modalOpen) {
+      return (
+        <>
+          <h2 className="table-title">Today</h2>
+          <table>
+            <thead>
+              <tr>
+                <th colSpan="3">{calories} calories remaining</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border-right">
+                  0
               <p>consumed</p>
-              </td>
-              <td className="border-right">
-                0
+                </td>
+                <td className="border-right">
+                  0
               <p>burned</p>
-              </td>
-              <td>
-                0
+                </td>
+                <td>
+                  0
               <p>net</p>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div className="modal-button-container">
-        <button className="modal-button" onClick={this.openModal}>
-          <i className="fas fa-plus"></i>
-        </button>
-        </div>
-      </>
-    );
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="modal-button-container">
+            <button className="modal-button" onClick={this.openModal}>
+              <i className="fas fa-plus"></i>
+            </button>
+          </div>
+          <div className="modal-container">
+            <div className="modal">
+              <div>
+              <i className="fas fa-utensils"></i>
+              <p>Food journal</p>
+              </div>
+              <div>
+              <i className="fas fa-dumbbell"></i>
+              <p>Workout journal</p>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h2 className="table-title">Today</h2>
+          <table>
+            <thead>
+              <tr>
+                <th colSpan="3">{calories} calories remaining</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border-right">
+                  0
+              <p>consumed</p>
+                </td>
+                <td className="border-right">
+                  0
+              <p>burned</p>
+                </td>
+                <td>
+                  0
+              <p>net</p>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="modal-button-container">
+            <button className="modal-button" onClick={this.openModal}>
+              <i className="fas fa-plus"></i>
+            </button>
+          </div>
+        </>
+      );
+    }
   }
 }
 
