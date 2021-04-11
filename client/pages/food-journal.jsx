a1c1477f0fea428fc70432e0827a6c399681443e
yesseniarod <yessi.rod30@gmail.com>
2021-04-11 22:40:54 +0000

    create search bar component. #3

--- client/pages/food-journal.jsx
+++ client/pages/food-journal.jsx
@@ -3,6 +3,15 @@ import React from 'react';
 export default function FoodJournal(props) {
   return (
     <>
+    <h2>Food journal</h2>
+    <form className="search">
+      <div className="searchbar">
+      <input type="search" placeholder="food search"/>
+        <button className="search-button">
+            <i className="fas fa-search"></i>
+        </button>
+        </div>
+    </form>
 
     </>
   );
