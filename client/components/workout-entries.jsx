417ad8f5766abd4a0160c395333b2c30297a1a4f
yesseniarod <yessi.rod30@gmail.com>
2021-04-16 20:33:51 +0000

    map through items array to display on page. #8

--- client/components/workout-entries.jsx
+++ client/components/workout-entries.jsx
@@ -29,6 +29,23 @@ class WorkoutEntries extends React.Component {
       <div className="entry-heading">
         <h2 className="workout-title">Workout journal</h2>
       </div>
+      <div className="search-result-container">
+        <ul className="entries-list">
+          <div className="result-list">
+            {
+              this.state.items.map(item => {
+                return <li key={item.workoutId}>
+                  <div className="exercise-entry-description">
+                    <p>{item.name}</p>
+                    <p>Duration: {item.duration} minutes</p>
+                    <p>Calories: {item.calories}</p>
+                  </div>
+                </li>;
+              })
+            }
+          </div>
+        </ul>
+      </div>
       </>
     );
   }
